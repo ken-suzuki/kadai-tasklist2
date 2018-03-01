3f19f1ae7c216f849efaf5fbdb1de05c0bf63e42
EC2 Default User <ec2-user@ip-172-31-14-134.us-east-2.compute.internal>
2018-03-01 05:09:53 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
 
-    <h1>メッセージ新規作成ページ</h1>
+    <h1>タスク新規作成ページ</h1>
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
 
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
