1a21ad5e6cd5a190c32f8ff115f30058f5cf276d
EC2 Default User <ec2-user@ip-172-31-14-134.us-east-2.compute.internal>
2018-03-11 08:16:01 +0000

    bootstrap design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,20 +19,14 @@
         <div class="container">
             <div class="row">
                 
-                <div class="col-xs-12">
-                <div class="col-sm-offset-2 col-sm-8">
-                <div class="col-md-offset-2 col-md-8">
-                <div class="col-lg-offset-3 col-lg-6">
+                <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
                     
             @include('commons.error_messages')
             
             @yield('content')
             
                 </div>
-                </div>
-                </div>
-                </div>
-                
+            
             </div>
         </div>
     </body>
